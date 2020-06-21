592ce9be19a1bf4e429f42e5ba6c2410314cd691
Camoo Dev <etn@camoo.sarl>
2020-06-21 17:04:00 +0200

    Stop/check running background process added

--- src/Console/BackgroundProcess.php
+++ src/Console/BackgroundProcess.php
@@ -8,11 +8,62 @@ use CAMOO\Exception\ConsoleException;
 final class BackgroundProcess
 {
     /** @var null|string $command */
-    private $command = null;
+    private $command;
+
+    /** @var int $pid */
+    private $pid = 0;
 
     public function __construct(?string $command = null)
     {
         $this->command  = $command;
+    }
+
+    /**
+     * @param int $pid
+     * @return void
+     */
+    private function setPid(int $pid) : void
+    {
+        $this->pid = $pid;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPid() : int
+    {
+        return $this->pid;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRunning() : bool
+    {
+        if (($pid = $this->getPid()) > 0) {
+            try {
+                $result = shell_exec(sprintf('ps %d 2>&1', $pid));
+                return count(preg_split("/\n/", $result)) > 2 && !preg_match('/ERROR: Process ID out of range/i', $result);
+            } catch (ConsoleException $exception) {
+                // Do nothing
+            }
+        }
+
+        return false;
+    }
+
+    public function stop() : bool
+    {
+        if (($pid = $this->getPid()) > 0) {
+            try {
+                $result = shell_exec(sprintf('kill -9 %d 2>&1', $pid));
+                return (boolean) !preg_match('/No such process/i', $result);
+            } catch (ConsoleException $exception) {
+                // Do nothing
+            }
+        }
+
+        return false;
     }
 
     protected function getCommand() : ?string
@@ -44,7 +95,10 @@ final class BackgroundProcess
             shell_exec(sprintf('%s &', $this->getCommand(), $sOutputFile));
             return 0;
         } elseif ($sOS === 'LINUX' || $sOS === 'FREEBSD' || $sOS === 'DARWIN') {
-            return (int) shell_exec(sprintf('%s %s %s 2>&1 & echo $!', $this->getCommand(), ($bAppend) ? '>>' : '>', $sOutputFile));
+            /** @var int */
+            $pid = (int) shell_exec(sprintf('%s %s %s 2>&1 & echo $!', $this->getCommand(), ($bAppend) ? '>>' : '>', $sOutputFile));
+            $this->setPid($pid);
+            return $pid;
         }
 
         throw new ConsoleException('Operating System not Supported');
