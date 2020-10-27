c1694eb101f473593a2c4cc680950883d8313961
Camoo Dev <etn@camoo.sarl>
2020-10-27 08:57:27 +0100

    Mailer: setDomain and addHeaders added

--- src/Mailer/Mailer.php
+++ src/Mailer/Mailer.php
@@ -17,31 +17,38 @@ use CAMOO\Exception\MailerException;
 /**
  * Class Message provides functionality to compose and send both text and MIME-compliant multipart email messages.
  *
- * @method Message setFrom(string $email, string $name = null) Sets the sender of the message. Email or format "John Doe" <doe@example.com>
+ * @method MessageWrapper setFrom(string $email, string $name = null) Sets the sender of the message. Email or format "John Doe" <doe@example.com>
  * @method null|array getFrom() Returns the sender of the message
- * @method Message addReplyTo(string $email, string $name = null) Adds the reply-to address. Email or format "John Doe" <doe@example.com>
- * @method Message setSubject(string $subject) Sets the subject of the message
+ * @method MessageWrapper addReplyTo(string $email, string $name = null) Adds the reply-to address. Email or format "John Doe" <doe@example.com>
+ * @method MessageWrapper setSubject(string $subject) Sets the subject of the message
  * @method null|string getSubject() Returns the subject of the message
- * @method Message addTo(string $email, string $name = null) Adds email recipient. Email or format "John Doe" <doe@example.com>
- * @method Message addCc(string $email, string $name = null) Adds carbon copy email recipient. Email or format "John Doe" <doe@example.com>
- * @method Message addBcc(string $email, string $name = null) Adds blind carbon copy email recipient. Email or format "John Doe" <doe@example.com>
- * @method Message setReturnPath(string $email) Sets the Return-Path header of the message
+ * @method MessageWrapper addTo(string $email, string $name = null) Adds email recipient. Email or format "John Doe" <doe@example.com>
+ * @method MessageWrapper addCc(string $email, string $name = null) Adds carbon copy email recipient. Email or format "John Doe" <doe@example.com>
+ * @method MessageWrapper addBcc(string $email, string $name = null) Adds blind carbon copy email recipient. Email or format "John Doe" <doe@example.com>
+ * @method MessageWrapper setReturnPath(string $email) Sets the Return-Path header of the message
  * @method null|string getReturnPath() Returns the Return-Path header
- * @method Message setPriority(int $priority) Sets email priority
+ * @method MessageWrapper setPriority(int $priority) Sets email priority
  * @method null|int getPriority() Returns email priority
- * @method Message setHtmlBody(string $html, string $basePath = null) Sets Html Body
+ * @method MessageWrapper setHtmlBody(string $html, string $basePath = null) Sets Html Body
  * @method string getHtmlBody() Gets HTML body
  * @method MimePart addEmbeddedFile(string $file, string $content = null, string $contentType = null) Adds embedded file
- * @method Message addInlinePart(MimePart $part) Adds inlined Mime Part
+ * @method MessageWrapper addInlinePart(MimePart $part) Adds inlined Mime Part
  * @method MimePart addAttachment(string $file, string $content = null, string $contentType = null) Adds Attachment
  * @method array getAttachments() Gets all email attachments
  * @method string generateMessage() Returns encoded message
- * @method Message build() Builds email. Does not modify itself, but returns a new object
- * @method Message setHeader(string $name, $value, bool $append = false) Sets a header
+ * @method MessageWrapper build() Builds email. Does not modify itself, but returns a new object
+ * @method MessageWrapper setHeader(string $name, $value, bool $append = false) Sets a header
  * @method mixed getHeader(string $name) Returns a header
- * @method Message clearHeader(string $name) Removes a header
+ * @method MessageWrapper clearHeader(string $name) Removes a header
  * @method null|string getEncodedHeader(string $name) Returns an encoded header
  * @method array getHeaders() Returns Headers
+ * @method MessageWrapper setContentType(string $contentType, string $charset = null) Sets Content-Type header
+ * @method MessageWrapper setEncoding(string $encoding) Sets Content-Transfer-Encoding header
+ * @method string getEncoding() Returns Content-Transfer-Encoding header
+ * @method MessageWrapper addPart(self $part = null) Adds or creates new multipart
+ * @method MessageWrapper setBody(string $body) Sets textual body
+ * @method string getBody() Gets textual body
+ * @method string getEncodedMessage() Returns encoded message
  *
  * @author CamooSarl
  */
@@ -52,6 +59,9 @@ class Mailer
 
     /** @var string $transport */
     private $transport;
+
+    /** @var null|string $domain */
+    private $domain;
 
     /** @var array */
     protected $defaultHeaders = [
@@ -90,9 +100,53 @@ class Mailer
         return $this->mail->__call($method, $arguments);
     }
 
+    /**
+     * Sets client Host. Important when emails are sending with background process
+     *
+     * @param string $domain
+     *
+     * @throw MailerException
+     *
+     * @return self
+     */
+    public function setDomain(string $domain) : self
+    {
+        if (!preg_match('/^(?:[a-zA-Z0-9]+(?:\-*[a-zA-Z0-9])*\.)+[a-zA-Z]{2,}$/', $domain)) {
+            throw new MailerException(sprintf('%s is not a valid domain', $domain));
+        }
+
+        $this->domain = $domain;
+        return $this;
+    }
+
+    /**
+     * Sets multiple headers
+     *
+     * @param array $headers
+     *
+     * @return void
+     */
+    public function addHeaders(array $headers) : void
+    {
+        foreach ($headers as $name => $value) {
+            $this->setHeader($name, $value);
+        }
+    }
+
+    /**
+     * Sends the message out via SMTP
+     *
+     * @throw MailerException
+     *
+     * @return void
+     */
     public function send() : void
     {
-        $smtpMailer = new SmtpMailer(Configure::read('SmtpTransport.' . $this->transport));
+        $mailOption = Configure::read('SmtpTransport.' . $this->transport);
+        if ($this->domain !== null) {
+            $mailOption['clientHost'] = $this->domain;
+        }
+        $smtpMailer = new SmtpMailer($mailOption);
         try {
             $from = $this->mail->getFrom();
             if ($from === null) {
